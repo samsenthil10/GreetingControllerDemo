ac3d38930b864ef13879e2cc74b4f8860f891930
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-01 04:13:18 +0530

    Added: To initialize project structure

---
+++ src/main/java/com/bridgelabz/greetingcontrollerdemo/GreetingControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcontrollerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingControllerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/greetingcontrollerdemo/GreetingControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingControllerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
